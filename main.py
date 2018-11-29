2c947f01337823fe7ea7100b0b6e6c37ac43bca6
Marcelo Fadini <marcelofadini@hotmail.com>
2018-11-29 09:23:48 -0200

    primeira versão de testes

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from mb import *
 
-if __name__ == __main__:
-    pass
+"""
+ivrequest
+
+Escreve 1 na flag de leitura de todos os escravos conectados ao barramento
+"""
+
+if __name__ == "__main__":
+    client = ModbusRtuMain(b'/dev/ttyUSB0', 38400, b'N', 8, 1)  #Baud=38400, 8N1 
+    client.setSlave(0x00)
+    try:                                       #Broadcast
+        client.mb.write_register( MbRegisters.READ_VAL_FLAG.value, 1)
+    except:
+        print("Falha na comunicação.") 
