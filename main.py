737c529ac2aee675b30ac6f25b15d57681080f5c
Marcelo Fadini <marcelofadini@hotmail.com>
2018-11-29 09:55:57 -0200

    removido o bug de erro no broadcast

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ Escreve 1 na flag de leitura de todos os escravos conectados ao barramento
 
 if __name__ == "__main__":
     client = ModbusRtuMain(b'/dev/ttyUSB0', 38400, b'N', 8, 1)  #Baud=38400, 8N1 
-    client.setSlave(0x00)
-    try:                                       #Broadcast
-        client.mb.write_register( MbRegisters.READ_VAL_FLAG.value, 1)
+    client.setSlave(0x00)                                       #Broadcast
+    try:
+        client.mb.write_register( MbRegisters.READ_VAL_FLAG.value , 1)
     except:
-        print("Falha na comunicação.") 
+        pass
