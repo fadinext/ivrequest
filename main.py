7aa8f15ddf64972d2468c618c1a393a3d1498555
Marcelo Fadini <marcelofadini@hotmail.com>
2018-11-29 13:08:55 -0200

    implementação dos arquivos de resultados

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 from mb import *
+from time import sleep
+from datetime import datetime,date
 
 """
 ivrequest
@@ -13,3 +16,44 @@ if __name__ == "__main__":
         client.mb.write_register( MbRegisters.READ_VAL_FLAG.value , 1)
     except:
         pass
+    
+   #sleep(2)
+    dirName = str( date.today())
+    try:
+        os.mkdir(dirName)
+    except:
+        pass
+
+    slaves = list(range(1,11))
+    
+    client.setSlave(10)
+    client.mb.read_input_registers( MbRegisters.VOLTAGE_INIT_REG.value , 1)
+
+    for j in range(1,11):
+        client.setSlave(j)
+        try:
+            client.mb.read_input_registers( MbRegisters.VOLTAGE_INIT_REG.value , 1)
+        except:
+            print(f"removido {j}")
+            slaves.remove(j)
+
+    print(slaves)
+    for i in slaves:
+        client.setSlave(i)
+        try:
+            x = list(client.mb.read_input_registers(MbRegisters.VOLTAGE_INIT_REG.value,
+                MbRegisters.DATA_REGS.value))
+            y = list(client.mb.read_input_registers(MbRegisters.CURRENT_INIT_REG.value, 
+                MbRegisters.DATA_REGS.value))
+        except:
+            with open(f"./{dirName}/{hex(i)}.txt","w") as f:
+                f.write('IO ERROR.')
+
+        with open(f"./{dirName}/{datetime.now()}{hex(i)}.txt","w") as f:
+                f.write('IO ERROR.')
+
+                        
+
+
+
+    
